def4e6e15bd8a9dcf8bb4468b4dbec4ffe162f71
mcopas <84601722+mcopas@users.noreply.github.com>
2021-05-31 18:40:18 -0400

    Finished lesson around Bean Scope

---
+++ src/main/java/guru/springframework/sfgdi/repositories/EnglishGreetingRepositoryImpl.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgdi.repositories;
+
+public class EnglishGreetingRepositoryImpl implements EnglishGreetingRepository{
+    @Override
+    public String getGreeting() {return "Hellow World - EN";}
+}
